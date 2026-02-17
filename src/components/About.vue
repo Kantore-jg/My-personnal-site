27519a833c42ec3475d482fb950c05fff5e1d7ff
Kantore-jg <codewithkantox@gmail.com>
2026-02-17 17:40:21 +0200

    bio

--- src/components/About.vue
+++ src/components/About.vue
@@ -24,7 +24,7 @@
                     <circle cx="12" cy="12" r="10"></circle>
                     <polyline points="12 6 12 12 16 14"></polyline>
                   </svg>
-                  Born June 3, 2005
+                  Born June 3
                 </span>
                 <span class="meta-item">
                   <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
